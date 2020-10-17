545f14171d7e1ce7b21fc80ae286bf8c42189268
Antwaun Johnson <antwaun.l.johnson@gmail.com>
2020-10-17 09:04:19 -0700

    setup initial component directories

---
+++ src/components/blog/blog-container.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+
+class BlogContainer extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Hello From the Blog</h1>
+      </div>
+    );
+  }
+}
+export default BlogContainer;
